5e278e0e1d98869bb34e0df971b33c2e16c3d18a
Danilo Fernandes <fernandes.danilo1996@gmail.com>
2022-01-08 12:52:10 -0400

    feat: Adiciona rota principal

---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import db from '../db/planets.json';
+
+const routes = Router();
+
+routes.get('/', async(req, res) => {
+  return res.json({
+    "/planets": "Lista todos os planetas",
+    "/planet:id": "Busca um planeta pelo seu id"
+  });
+});
