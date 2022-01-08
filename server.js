6e4cfb2de4455a36a5ec13efe96b68e082921529
Danilo Fernandes <fernandes.danilo1996@gmail.com>
2022-01-08 12:58:07 -0400

    feat: Adiciona rota de busca de planeta por id

--- server.js
+++ server.js
@@ -5,3 +5,4 @@ const app = express();
 app.listen(3333, () => {
   console.log('Servidor rodando na porta 3333');
 });
+
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,13 +4,22 @@ import db from '../db/planets.json';
 
 const routes = Router();
 
-routes.get('/', async(req, res) => {
+routes.get('/', (req, res) => {
   return res.json({
     "/planets": "Lista todos os planetas",
     "/planet:id": "Busca um planeta pelo seu id"
   });
 });
 
-routes.get('/planets', async(req, res) => {
+routes.get('/planets', (req, res) => {
   return res.json(db.planets);
 });
+
+routes.get('/planet/:id', (req, res) => {
+  const { id } = req.params;
+  const planetId = db.planets.findIndex( planet => planet.id === id );
+  const planet = db.planets[planetId];
+  return res.json(planet);
+});
+
+export default routes;
