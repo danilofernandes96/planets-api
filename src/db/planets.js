37eda3877acb73b4baa372d2c7c08e44d9c20137
Danilo Fernandes <fernandes.danilo1996@gmail.com>
2022-01-12 14:20:56 -0400

    feat: Adiciona temperatura e tamanho dos planetas

--- src/db/planets.js
+++ src/db/planets.js
@@ -3,56 +3,74 @@ export default {
     {
       "id": "1",
       "name": "Mercúrio",
-      "curiosity": "As temperaturas no planeta podem atingir 420 ºC de máxima e -173 ºC de mínima, sendo o planeta com a maior oscilação térmica do Sistema Solar.",
-      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/mercury.svg"
+      "curiosity": "As temperaturas no planeta podem atingir 427 ºC de máxima e -173 ºC de mínima, sendo o planeta com a maior oscilação térmica do Sistema Solar.",
+      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/mercury.svg",
+      "temperature": "427°C",
+      "size": "4.879,4 km"
     },
     {
       "id": "2",
       "name": "Vênus",
       "curiosity": "O único planeta do nosso sistema solar que gira no sentido anti-horário – se passássemos um dia inteiro por lá, veríamos o Sol nascer no oeste e se por no leste, ao contrário do que acontece na Terra.",
-      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/venus.svg"
+      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/venus.svg",
+      "temperature": "484°C",
+      "size": "12.104 km"
     },
     {
       "id": "3",
       "name": "Terra",
       "curiosity": " A temperatura no núcleo da Terra é elevadíssima: em torno de 5 mil graus célsius.",
-      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/earth.svg"
+      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/earth.svg",
+      "temperature": "14°C",
+      "size": "12.742 km"
     },
     {
       "id": "4",
       "name": "Marte",
       "curiosity": "Como o planeta vermelho tem menos gravidade do que a Terra, o peso de uma pessoa seria 62% menor por lá e a capacidade de levantar pesos fica duas vezes maior: cada quatro quilos na Terra correspondem a pouco menos que dois quilos em Marte.",
-      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/mars.svg"
+      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/mars.svg",
+      "temperature": "-63°C",
+      "size": "6.779 km"
     },
     {
       "id": "5",
       "name": "Júpiter",
       "curiosity": "Trata-se de um planeta sem superfície sólida, o que significa que seria impossível fincar uma bandeira tal como Neil Armstrong e Buzz Aldrin, os primeiros astronautas a pisar na Lua, fizeram. Na verdade, não seria possível nem pisar em Júpiter.",
-      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/jupiter.svg"
+      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/jupiter.svg",
+      "temperature": "-110°C",
+      "size": "139.820 km"
     },
     {
       "id": "6",
       "name": "Saturno",
       "curiosity": "Olhando da Terra, Saturno é um dos 5 planetas visíveis a olho nu. Então, se Saturno estiver no céu à noite, você pode sair e vê-lo.",
-      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/saturn.svg"
+      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/saturn.svg",
+      "temperature": "-138°C",
+      "size": "116.460 km"
     },
     {
       "id": "7",
       "name": "Urano",
       "curiosity": "Apesar de não ser o planeta mais distante do Sol, Urano ainda é o planeta mais frio do Sistema Solar, com uma temperatura atmosférica mínima de -224 °C. Uma das teorias que explica o porquê de Urano ser tão frio diz que o planeta pode ter passado por uma grande colisão, que fez com o calor do seu núcleo escapasse para o espaço.",
-      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/uranus.svg"
+      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/uranus.svg",
+      "temperature": "-197°C",
+      "size": "50.724 km"
     },
     {
       "id": "8",
       "name": "Netuno",
       "curiosity": "Com ventos que chegam à 2.100 km/h. Os cientistas ainda se perguntam como um planeta frio e gelado como Netuno consegue ter ventos tão rápidos. Uma ideia é que as temperaturas frias e o fluxo de gases fluidos em sua atmosfera fazem com que seja fácil gerar ventos que se movam tão rapidamente.",
-      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/neptune.svg"
+      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/neptune.svg",
+      "temperature": "-245°C",
+      "size": "49.244 km"
     },
     {
       "id": "9",
       "name": "Plutão",
       "curiosity": "Plutão foi considerado o nono planeta a orbitar o Sol até 2006, quando passou a ser classificado como planeta anão pela União Astronômica Internacional.",
-      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/pluto.svg"
+      "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/pluto.svg",
+      "temperature": "-230°C",
+      "size": "2.376,6 km"
     }
   ]
 }
