c68a6323d0df9b80c39cdaf930c30a5a528a0d05
Danilo Fernandes <fernandes.danilo1996@gmail.com>
2022-01-19 13:28:33 -0400

    feat: Implementa dados adicionais aos planetas

--- src/db/planets.js
+++ src/db/planets.js
@@ -6,7 +6,10 @@ export default {
       "curiosity": "As temperaturas no planeta podem atingir 427 ºC de máxima e -173 ºC de mínima, sendo o planeta com a maior oscilação térmica do Sistema Solar.",
       "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/mercury.svg",
       "temperature": "427°C",
-      "size": "4.879,4 km"
+      "size": "4.879,4 km",
+      "distance_sun": "57.910.000 km",
+      "duration_day": "58d 15h 30m",
+      "orbital_period": "88 dias"
     },
     {
       "id": "2",
@@ -14,15 +17,21 @@ export default {
       "curiosity": "O único planeta do nosso sistema solar que gira no sentido anti-horário – se passássemos um dia inteiro por lá, veríamos o Sol nascer no oeste e se por no leste, ao contrário do que acontece na Terra.",
       "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/venus.svg",
       "temperature": "484°C",
-      "size": "12.104 km"
+      "size": "12.104 km",
+      "distance_sun": "108.200.000 km",
+      "duration_day": "116d 18h 0m",
+      "orbital_period": "225 dias"
     },
     {
       "id": "3",
       "name": "Terra",
-      "curiosity": " A temperatura no núcleo da Terra é elevadíssima: em torno de 5 mil graus célsius.",
+      "curiosity": "A temperatura no núcleo da Terra é elevadíssima: em torno de 5 mil graus célsius. De acordo com estudos científicos, o planeta Terra surgiu a cerca de 4,5 bilhões de anos.",
       "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/earth.svg",
       "temperature": "14°C",
-      "size": "12.742 km"
+      "size": "12.742 km",
+      "distance_sun": "149.598.262 km",
+      "duration_day": "23h 56m",
+      "orbital_period": "365 dias"
     },
     {
       "id": "4",
@@ -30,7 +39,10 @@ export default {
       "curiosity": "Como o planeta vermelho tem menos gravidade do que a Terra, o peso de uma pessoa seria 62% menor por lá e a capacidade de levantar pesos fica duas vezes maior: cada quatro quilos na Terra correspondem a pouco menos que dois quilos em Marte.",
       "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/mars.svg",
       "temperature": "-63°C",
-      "size": "6.779 km"
+      "size": "6.779 km",
+      "distance_sun": "227.900.000 km",
+      "duration_day": "1d 0h 37m",
+      "orbital_period": "687 dias"
     },
     {
       "id": "5",
@@ -38,7 +50,10 @@ export default {
       "curiosity": "Trata-se de um planeta sem superfície sólida, o que significa que seria impossível fincar uma bandeira tal como Neil Armstrong e Buzz Aldrin, os primeiros astronautas a pisar na Lua, fizeram. Na verdade, não seria possível nem pisar em Júpiter.",
       "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/jupiter.svg",
       "temperature": "-110°C",
-      "size": "139.820 km"
+      "size": "139.820 km",
+      "distance_sun": "778.500.000 km",
+      "duration_day": "0d 9h 56m",
+      "orbital_period": "12 anos"
     },
     {
       "id": "6",
@@ -46,7 +61,10 @@ export default {
       "curiosity": "Olhando da Terra, Saturno é um dos 5 planetas visíveis a olho nu. Então, se Saturno estiver no céu à noite, você pode sair e vê-lo.",
       "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/saturn.svg",
       "temperature": "-138°C",
-      "size": "116.460 km"
+      "size": "116.460 km",
+      "distance_sun": "1.429.400.000 km",
+      "duration_day": "0d 10h 42m",
+      "orbital_period": "29 anos"
     },
     {
       "id": "7",
@@ -54,7 +72,10 @@ export default {
       "curiosity": "Apesar de não ser o planeta mais distante do Sol, Urano ainda é o planeta mais frio do Sistema Solar, com uma temperatura atmosférica mínima de -224 °C. Uma das teorias que explica o porquê de Urano ser tão frio diz que o planeta pode ter passado por uma grande colisão, que fez com o calor do seu núcleo escapasse para o espaço.",
       "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/uranus.svg",
       "temperature": "-197°C",
-      "size": "50.724 km"
+      "size": "50.724 km",
+      "distance_sun": "2.870.990.000 km",
+      "duration_day": "0d 17h 14m",
+      "orbital_period": "84 anos"
     },
     {
       "id": "8",
@@ -62,7 +83,10 @@ export default {
       "curiosity": "Com ventos que chegam à 2.100 km/h. Os cientistas ainda se perguntam como um planeta frio e gelado como Netuno consegue ter ventos tão rápidos. Uma ideia é que as temperaturas frias e o fluxo de gases fluidos em sua atmosfera fazem com que seja fácil gerar ventos que se movam tão rapidamente.",
       "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/neptune.svg",
       "temperature": "-245°C",
-      "size": "49.244 km"
+      "size": "49.244 km",
+      "distance_sun": "4.504.300.000 km",
+      "duration_day": "0d 16h 6m",
+      "orbital_period": "165 anos"
     },
     {
       "id": "9",
@@ -70,7 +94,10 @@ export default {
       "curiosity": "Plutão foi considerado o nono planeta a orbitar o Sol até 2006, quando passou a ser classificado como planeta anão pela União Astronômica Internacional.",
       "image": "https://raw.githubusercontent.com/gist/danilofernandes96/5a57604adf820b6618fb59337e755ba0/raw/d28696e2a095156a57d09ace678331e599c0c25b/pluto.svg",
       "temperature": "-230°C",
-      "size": "2.376,6 km"
+      "size": "2.376,6 km",
+      "distance_sun": "5.913.520.000 km",
+      "duration_day": "6d 5h 0m",
+      "orbital_period": "248 anos"
     }
   ]
 }
